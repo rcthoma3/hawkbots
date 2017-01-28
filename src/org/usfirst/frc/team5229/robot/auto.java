0029a445e77ccf555cc5756829e54d3f7b7d9c81
gilbecon000 <gilbecon000@HHS-308-06.stcs.org>
2017-01-28 11:24:47 -0500

    BrianC: skelton Code

--- src/org/usfirst/frc/team5229/robot/auto.java
+++ src/org/usfirst/frc/team5229/robot/auto.java
@@ -1,5 +1,48 @@
 package org.usfirst.frc.team5229.robot;
 
 public class auto {
+	
+	//Ideally shoots ball
+	//in: nothing
+	//out: nothing
+	public void shootball() {
+		
+	}
+	//drives forward
+	//in: nothing
+	//out: nothing
+	public void moveforward() {
+		
+	}
+	// the robot lines up to shoot the ball at the top of boiler
+	//in: nothing
+	//out: nothing
+	public void lineup() {
+		
+	}
+	// the robot drives distance then turns left
+	//in: nothing
+	//out: nothing 
+	public void turnleft() {
+		
+	}
+    //the robot drives distance them turns right
+	//in: nothing
+	//out: nothing
+	public void turnright() {
+		
+	}
+	//the robot gets the distance using the sensor then drops the gear once it gets to the gear pully
+	//in: distance
+	//out: gear drop
+	public void dropgear() {
+		
+	}
 
+	//the robot picks up the gear
+	//in: nothing
+	//out: gear
+	public void pickupgear() {
+		
+	}
 }
