2ee641b661c987bca194cb1a8cd336257854ad41
poweladi000 <poweladi000@HHS-308-13.stcs.org>
2017-01-28 12:57:48 -0500

    Adiah: Skeleton code

--- src/org/usfirst/frc/team5229/robot/Controller.java
+++ src/org/usfirst/frc/team5229/robot/Controller.java
@@ -2,4 +2,146 @@ package org.usfirst.frc.team5229.robot;
 
 public class Controller {
 
+	//Is the A button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isAPressed() {		
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is the Right Trigger button pressed or not
+	//in: nothing
+	//out: is a button pressed, float
+	public float RightTrigger() {
+		float rt=0;
+		return rt;
+		
+	}
+	
+	//Is the B button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isBPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is the X button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isXPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is the Y button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isYPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is Up D pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isUpDPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is Down D button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isDownDPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is Right D pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isRightDPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is Left D pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isLeftDPressed() {
+		boolean isPressed = false;
+		
+		
+		return isPressed;
+		
+		
+	}
+	
+	//Is the Left Trigger button pressed or not
+	//in: nothing
+	//out: is a button pressed, float
+	public float leftTrigger() {
+		float rt=0;
+		return rt;
+		
+	}
+	
+	//Is the Right Joy button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isRJoyPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+	
+	}
+	
+	//Is the Left Joy button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isLJoyPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is LB button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isLBPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+		
+	}
+	
+	//Is RB button pressed or not
+	//in: nothing
+	//out: is a button pressed, boolean
+	public boolean isRBPressed() {
+		boolean isPressed = false;
+		
+		return isPressed;
+	}
+	
 }
+	
+	
+
