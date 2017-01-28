fa6947d63dd88314a4f83dab40979cdbfb3f8dca
devosseb000 <devosseb000@HHS-308-03.stcs.org>
2017-01-28 10:44:18 -0500

    Sebastian; Skeleton of the sensor code

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -10,5 +10,39 @@ public class Sensors {
 		//Code to find distance goes here
 		
 		return distance;
+		
+	}
+	//Provides distance between the robot and the wall from the side
+	//in inches
+	//In: nothing
+	//Out: distance as float.
+	
+	public float SideSensors() {
+		float distance = 0;
+		//Distance goes here
+		return distance;
+	}
+	//Color helps tell the robot what team it is on
+	//In: nothing
+	//Out: Color as int
+	public int ColorSensors() {
+		int color = 0;
+		//Color in code goes here
+		return color;
+	}
+	//This lets side and front sensors to tell the angle of how close the wall is
+	//In: nothing
+	//Out: angle 
+	public float angle() {
+		float angle = 0;
+		return angle;
+		
+	}
+	//Lets sensors be able to tell if a wall is too close on one side
+	//In: nothing
+	//Out: distance/angle to wall
+	public float sensors() {
+		float distance = 0;
+		return distance;
 	}
 }
