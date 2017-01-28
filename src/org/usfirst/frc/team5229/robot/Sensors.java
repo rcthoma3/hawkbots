f272f9164f22a6662a74ffd0df44a35813a52d17
bjcobb <bjcobb@HHS-308-25.stcs.org>
2017-01-28 09:56:10 -0500

    BrianC: Added front sensor code

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -1,5 +1,14 @@
 package org.usfirst.frc.team5229.robot;
 
 public class Sensors {
-
+	//Provides the distance between the robot and
+	//a wall in front of it in inches
+	//In: nothing
+	//Out: distance as float
+	public float FrontSensors() {
+		float distance = 0;
+		//Code to find distance goes here
+		
+		return distance;
+	}
 }
