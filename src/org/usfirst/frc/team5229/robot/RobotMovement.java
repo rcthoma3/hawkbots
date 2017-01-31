5f5863a7af20107699c7197f91f249ba39e34fb6
panugami000 <panugami000@HHS-308-15.stcs.org>
2017-01-30 20:32:38 -0500

    Drive Team:Final Comments

--- src/org/usfirst/frc/team5229/robot/RobotMovement.java
+++ src/org/usfirst/frc/team5229/robot/RobotMovement.java
@@ -10,7 +10,7 @@ public class RobotMovement {
 	private boolean modeFine = true; 
     private boolean squaredInputs = true; 
     private static double wheelBase = 24.0;
-    public float outputMagnitude;
+    
     
     private Sensors sensors = new Sensors();
    
@@ -18,19 +18,23 @@ public class RobotMovement {
 	public RobotMovement(){
 	}
 	
-	
+	//in: newWheelBase
+	//out:nothing
 	public RobotMovement(double newWheelBase){
 		wheelBase = newWheelBase;
 		//wheelBase is the distance from the middle of the right wheel to the middle of the left wheel
 	}
 	
+	//in:Joystick Left, Joystick Right
+	//out: nothing
 	public RobotMovement(Joystick Left, Joystick Right){
 		//set left joystick set right joystick
 		SetLeftJoystick(Left);
 		SetRightJoystick(Right);
 	}
 	
-
+    //in:Joystick Left, Joystick Right, newWheelBase
+	//out:nothing
 	public RobotMovement(Joystick Left, Joystick Right, double newWheelBase){
 		//set left joystick set right joystick
 		SetLeftJoystick(Left);
@@ -46,22 +50,28 @@ public class RobotMovement {
 	
 	
     //set left stick//
+    //in:Joystick lJoystick
+    //out:nothing
     public void SetLeftJoystick(Joystick lJoystick){
     	leftStick = lJoystick;
 
     }
     
     //set right stick//
+    //in:Joystick rJoystick
+    //out:nothing
     public void SetRightJoystick(Joystick rJoystick) {
     	rightStick = rJoystick;
     }
 	
+    //in:newWheelBase
+    //out:nothing
    public void createWheelBase(double newWheelBase){
 	wheelBase = newWheelBase;
 		//wheelBase is the distance from the middle of the right wheel to the middle of the left wheel
 	} 
     //set drive forward and backward//
-    //in: nothing
+    //in: speed
     //out: nothing
     //speed>0, moves forward
     //speed<0, moves backward
@@ -76,6 +86,8 @@ public class RobotMovement {
     	
     }
     
+    //in:r
+    //out:Math.exp(-r/wheelBase)
     private double rToCurve(double r){
     	return Math.exp(-r/wheelBase);
 	//takes the the wheelBase of a robot and the radius of the circle that the curve would be part of and imputs
@@ -83,6 +95,8 @@ public class RobotMovement {
     }
     
   //how far the outside wheel(right wheel) is going
+    //in:r,angdeg
+    //out:(r+wheelBase/2)*Math.toradians(angdeg)
     public double angleToTurnDistance(double r, double angdeg){
 	   return (r+wheelBase/2)*Math.toRadians(angdeg);
     }
@@ -102,57 +116,79 @@ public class RobotMovement {
     }
     
     //tell what the is speed//
+    //in:speed
+    //out:speed
     public int whatisSpeed(int speed){
     	return speed;
     }
     
     //tell how many degrees did the robot turn//
+    //in:r
+    //out:rToCurve(r)
     public double whatisDegree(double r){
     	return rToCurve(r);
     }
     
     //set mode to arcadeDrive//
+    //in:nothing
+    //out:nothing
 	public void setmodeArcade(){
 		modeArcade = true; 
 	}
 	
 	//set mode to tankDrive//
+	//in:nothing
+	//out:nothing
 	public void setmodeTank() {
 		modeArcade = false;
 	}
 
 	//set mode to Fine//
+	//in:nothing
+	//out:nothing
 	public void setmodeFine(){
 		modeFine = true;
 	}
 	
 	//set mode to Coarse//
+	//in:nothing
+	//out:nothing
 	public void setmodeCoarse(){
 		modeFine = false; 
 	}
 	
 	//tell that mode is arcadeDrive//
+	//in:nothing
+	//out:modeArcade
 	public boolean ismodeArcade(){
 		return modeArcade;		
 	}
 	
 	//tell that mode is tankDrive//
+	//in:nothing
+	//out:! modeArcade
 	public boolean ismodeTank(){
 		return ! modeArcade; 
 	}
 	
 	//tell that mode is Fine//
+	//in:nothing
+	//out:modeFine
 	public boolean ismodeFine(){
 		return modeFine;
 		
 	}
 	
 	//tell that mode is Coarse//
+	//in:nothing
+	//out:nothing
 	public boolean ismodeCoarse(){
 		return !modeFine; 
 	}
 	
 	//do the type of drive and how the drive work//
+	//in:nothing
+	//out:nothing
 	public void doDriveType(){
 	
 		if(modeArcade == true && modeFine == true){
@@ -168,6 +204,8 @@ public class RobotMovement {
 	}
 	
 	//Stop when there is something forward//
+	//in:speed
+	//out:nothing
 	public void drivefowardunsafe(double speed){
 		float distance = sensors.FrontSensors();
 		if(distance==1 && speed>0){
