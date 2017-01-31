98e11682d1387a330df0a827cd3595b11fe68447
devosseb000 <devosseb000@HHS-308-03.stcs.org>
2017-01-30 20:30:41 -0500

    Sebastian-Sensor Team
    Added more code to the outline for the camera.

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -1,6 +1,52 @@
 package org.usfirst.frc.team5229.robot;
 
-public class Sensors {
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+public class Sensors extends IterativeRobot {
+	Thread visionThread;
+	@Override
+	public void robotInit() {
+		visionThread = new Thread(() -> {
+			// Get UsbCamera from camera server
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			//Set up resolution
+			camera.setResolution(640, 480);
+			//Get a CvSink. This will capture Mats from camera
+		CvSink cvSink = CameraServer.getInstance().getVideo();
+		//Setup a CvSource. This will send images back to the dashboard
+		CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+		//Mats are very memory expensive. Let's reuse this Mat.
+		Mat mat = new Mat();
+		// This cannot be 'true'. The program will never exit if it is. This
+					// lets the robot stop this thread when restarting robot code or
+					// deploying.
+		while (!Thread.interrupted()) {
+			// Tell the CvSink to grab a frame from the camera and put it
+			// in the source mat.  If there is an error notify the output.
+			if (cvSink.grabFrame(mat) == 0) {
+				//Send the output the error
+			outputStream.notifyError(cvSink.getError());
+			//Skip the rest of the current iteration
+			continue;
+			}
+			//Put a rectangle on the image
+			Imgproc.rectangle(mat, new Point(100,100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+			//Give out[ut stream a new image to display
+			outputStream.putFrame(mat);
+		}
+	});
+		visionThread.setDaemon(true);
+		visionThread.start();
+	}
 public Sensors() {}
 	//Provides the distance between the robot and
 	//a wall in front of it in inches
