3a432173ca96f581ac8b426882588cade8a686e1
devosseb000 <devosseb000@HHS-308-03.stcs.org>
2017-02-04 13:07:01 -0500

    Sebastian-Sensor team
    UltraSensor code added

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team5229.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.RobotDrive;
 
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -10,8 +13,52 @@ import org.opencv.imgproc.Imgproc;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.CameraServer;
 public class Sensors extends IterativeRobot {
+	//Distance in inches the robot wants to stay from object,
+	private static final double kHoldDistance = 12.0;
+	//Max distance in inches we expect the robot to see
+	private static final double kMaxDistance = 24.0;
+	//factor to convert sensor value to distance  in inches
+	private static final double kValueToInches = 0.125;
+	//Proportional speed constant
+	private static final double kP = 0.05;
+	//Integral speed constant 
+	private static final double kI = 0.018;
+	//Derivative speed constant
+	private static final double kD = 1.5;
+	
+	private static final int kLeftMotorPort = 0;
+	private static final int kRightMotorPort = 1;
+	private static final int kUltrasonicPort = 0;
+	
+	private AnalogInput ultrasonic = new AnalogInput(kUltrasonicPort);
+	private RobotDrive myRobot = new RobotDrive(kLeftMotorPort, kRightMotorPort);
+	private PIDController pidController = new PIDController(kP, kI, kD, ultrasonic, new MyPidOutput());
+	@Override
+	public void teleopInit() {
+		//Set expected range to 0-24 inches; e.g. at from object go
+		//full forward, at 0 inches from object go full backwards
+		pidController.setInputRange(0, kMaxDistance * kValueToInches);
+		//Set setpoint  of the pidController
+		pidController.setSetpoint(kHoldDistance * kValueToInches);
+		pidController.enable(); //Begin PID control
+	}
+	private class MyPidOutput implements PIDOutput {
+		@Override
+		public void pidWrite(double output) {
+			myRobot.drive(output, 0);
+		}
+	}
+	public void teleopPeriodic() {
+		//Sensor returns a value from 0-4095 that is scaled to inches
+		double currentDistance = ultrasonic.getValue() * kValueToInches;
+		//Convert distance error to a motor speed
+		double currentSpeed = (kHoldDistance - currentDistance) * kP;
+		myRobot.drive(currentSpeed, 0);
+	}
+	
 	Thread visionThread;
 	@Override
 	public void robotInit() {
