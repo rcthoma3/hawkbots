b3ba6525ec21c0d376961926966a661122590619
gilbecon000 <gilbecon000@HHS-308-06.stcs.org>
2017-03-18 13:24:27 -0400

    ConnorG: fixed entire auto code lol

--- src/org/usfirst/frc/team5229/robot/auto.java
+++ src/org/usfirst/frc/team5229/robot/auto.java
@@ -2,179 +2,22 @@ package org.usfirst.frc.team5229.robot;
 
 import edu.wpi.first.wpilibj.Timer;
 
-public class auto {	
+public class auto {
 	RobotMovement robot = new RobotMovement();
 	Sensors sensors = new Sensors();
 	Timer AutoTimer = new Timer();//timer for automonous 
 	public boolean AutoTesting;//start auto test funtcion
+	public boolean Sensordetecting = false; //boolean for autotesting statement
+	double value;
+	double value2;
 	
-	private final int RED=0;
-	private final int BLUE=1;
-	private int mySide=-1;
-	
-	//States
-	public enum STATES {
-		STOP,
-		PICKUP_GEAR,
-		DROP_GEAR,
-		SHOOT_BALL,
-		FIELD_READ,
-		HOPPER,
-		PICK_UP_BALLS,
-		Exit,
+	public void straightenOut() {
 		
-	}
-	
-	STATES state = STATES.STOP;
-	boolean hasGear = false;
-	boolean hasBalls = false;
-	boolean noBalls = false;
-	boolean Robot = false; 
-	boolean noGear = false;
-	
-	
-	public auto() {
-	
-	}
-	//Finds the burner and which team we are on.
-	public void findBurner() {
-		robot.turnRight(50, 90);
-		int color = -1;
-		while(color != RED && color != BLUE) {
-			robot.DrivefowardBackward(100);
-			color = sensors.ColorSensors();
-			//if (sensors.FrontSensors()<1) {
-			//	robot.turnLeft(50, 180);
-			//}			
-		}
-		mySide=color;
-	}
-	//Ideally shoots ball
-	//in: nothing
-	//out: nothing
-	public void shootball() {
-		//Step 1:Use sensor to line up robot straight towards burner
-		//sensors.lineUp();
-		 double distanceToGoal = 0;
-		 distanceToGoal = (sensors.SonicLeftCenter() + sensors.SonicRightCenter())/2;
-		 while (distanceToGoal>80) {
-			 robot.DrivefowardBackward(50);
-			 distanceToGoal = (sensors.SonicLeftCenter() + sensors.SonicRightCenter())/2;
-		 }
-		 //robot.dumpBalls();
-		 robot.DrivefowardBackward(-50);
-	}
-	// the robot lines up to shoot the ball at the top of boiler
-	//in: nothing
-	//out: nothing
-	public void lineupwithburner() {
-		double left = sensors.SonicLeftCenter();
-		double right = sensors.SonicRightCenter();
-		while (Math.abs(left - right) > 20 ) {
-			if (left > right)
-				robot.turnRight(50, 10);
-			else
-				robot.turnLeft(50, 10);
-			left = sensors.SonicLeftCenter();
-			right = sensors.SonicRightCenter();
-		}		
-	}//
-	//the robot gets the distance using the sensor then drops the gear once it gets to the gear pully
-	//in: distance
-	//out: gear drop
-	public void dropgear() {
-		//Step 1: Have front sensors get distance from wall
-		double left = sensors.SonicLeftCenter();
-		double right = sensors.SonicRightCenter();
-		//Step 2: Line up by making distances equal
-		while (Math.abs(left - right) > 20) {
-			if (left > right)
-				robot.turnRight(50, 10);
-			else 
-				robot.turnLeft(50, 10);
-			left = sensors.SonicLeftCenter();
-			right = sensors.SonicRightCenter();
-		}
-		//Step 3: Move forward certain distance from gear drop location
-		double distanceToWall = 0;
-		distanceToWall = (sensors.SonicLeftCenter() + sensors.SonicRightCenter())/2;
-		while(distanceToWall>80) {
-			robot.DrivefowardBackward(50);
-			distanceToWall = (sensors.SonicLeftCenter() + sensors.SonicRightCenter())/2;
-		}
-		//Step 4: Drop gear onto peg
+		if (sensors.SonicRightCenter() > sensors.SonicLeftCenter())
+			robot.turnLeft(.25, 1);
 		
-	}
-
-	//the robot picks up the gear
-	//in: nothing
-	//out: gear
-	public void pickupgear() {
-		//Step 1: Drive forward till sensor picks up distance
-		//Step 2: use sensor to reposition so that robot is straight 
-		//Step 3: Drive forward until a certain distance from drop off is reached
-		//Step 4: Wait for drop off then back up
-		
-		
-	}
-	public void findhopperfromburner() {
-		if (mySide==RED) 
-		{
-			robot.turnRight(50,90);
-			robot.DrivefowardBackward(50);
-		}
-		else 
-		{ 
-				robot.turnLeft(50,90);
-				robot.DrivefowardBackward(50);
-				
-		}
-	}
-	//the robot follows a pick up pattern for fuel
-	//in: pattern
-	//out: balls
-	public void followpickuppattern() {
-		robot.turnLeft(50,90);
-		robot.DrivefowardBackward(50);
-		robot.turnRight(50,90);
-		robot.DrivefowardBackward(50);
-		robot.turnRight(50,90);
-		robot.DrivefowardBackward(50);
-		robot.turnRight(50,90);
-		robot.DrivefowardBackward(50);
-	}
-		
-		
-		
-	
-	
-	public void run() {
-		if (state==STATES.STOP) {
-			if (hasBalls) {
-				state = STATES.PICKUP_GEAR;
-			}
-		} //End stop state		
-		else if (state==STATES.PICKUP_GEAR) {
-			if (hasGear) {
-				state = STATES.DROP_GEAR;
-			}
-		}//End pickup gear state
-		else if (state==STATES.DROP_GEAR) {
-			if (noGear) {
-				state = STATES.FIELD_READ;
-			}
-		} //End drop gear
-	
-		else if (state==STATES.HOPPER) {
-			if (hasBalls){
-				state = STATES.SHOOT_BALL;
-			}
-		}//End hopper state
-		else if (state==STATES.SHOOT_BALL) {
-			if (noBalls) {
-				state = STATES.Exit;
-			}
-		}//End shoot ball
+		else if (sensors.SonicLeftCenter() > sensors.SonicRightCenter())
+			robot.turnRight(.25, 1);
 	}
 	
 	public boolean StartAutoTimer(){
@@ -185,51 +28,31 @@ public class auto {
 		return true;
 	}
 	
+	
+	
 	public void AutoTesting(){
-		if(AutoTimer.get() < 1.0){
+		if(AutoTimer.get() < 2.0){
+			if(sensors.SonicRightCenter() > value)
+				robot.DrivefowardBackward(1.0);
+			else{
+				robot.DrivefowardBackward(0);
+				Sensordetecting = true;		
+			}
 			
-		}else if(AutoTimer.get() <2.0){
-			robot.DrivefowardBackward(0);
 			
-		}else if(AutoTimer.get() <3.0){
-			
-		}else if(AutoTimer.get() < 4.0){
-			robot.DrivefowardBackward(0);
-			
-		}else if(AutoTimer.get() < 5.0){
-			
-		}else if(AutoTimer.get() < 6.0){
-			robot.DrivefowardBackward(0);
-			
-		}else if(AutoTimer.get() < 7.0){
-			
-		}else if(AutoTimer.get() < 8.0){
-			robot.DrivefowardBackward(0);
-			
-		}else if(AutoTimer.get() < 9.0){
-			
-		}else if(AutoTimer.get() < 10.0){
-			robot.DrivefowardBackward(0);
-			
-		}else if(AutoTimer.get() < 11.0){
-			
-		}else if(AutoTimer.get() < 12.0){
-			robot.DrivefowardBackward(0);
-			
-		}else if(AutoTimer.get() < 13.0){
-			
-		}else if(AutoTimer.get() < 14.0){
-			robot.DrivefowardBackward(0);
-			
-		}else if(AutoTimer.get() < 15.0){
-			
-		}else if(AutoTimer.get() == 15.0){
-			robot.DrivefowardBackward(0);
+		}else if(AutoTimer.get() <4.0 && Sensordetecting == true){
+			//robot.DrivefowardBackward(0);
+			//Align
+				
+		}else if(AutoTimer.get() <6.0 && Sensordetecting == true){
+			if (sensors.SonicRightCenter() > value2)
+			robot.DrivefowardBackward(1);
 			
 			AutoTesting = false;
 			AutoTimer.reset();
+			
+			
+
 		}
 	}
-	
-	
-}
+}	
