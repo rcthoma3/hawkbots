dbea74b5cf3e10e252bdcd4b5d88e1ef645d3950
panugami000 <panugami000@HHS308-25.stcs.org>
2018-01-13 15:00:10 -0500

    Sensors: Added Encoders and Autonomous Forward

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -1,7 +1,21 @@
 package org.usfirst.frc.team5229.robot;
 
-public class Sensors {
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+public class Sensors {
+	Encoder LeftFrontEnc = new Encoder(0, 1, false, Encoder.EncodingType.k4X );
+	Encoder LeftRearEnc = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
+	Encoder RightFrontEnc = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
+	Encoder RightRearEnc = new Encoder(6, 7, false , Encoder.EncodingType.k4X);
+	SpeedController D;
+	SpeedController E;
+	SpeedController F;
+	SpeedController G;
+	MecanumDrive drive = new MecanumDrive(D, E, F, G);
+	double encoderDistanceReading;
 	//Gets robot position
 	public void getPosition (double distance) {
 		
@@ -9,5 +23,17 @@ public class Sensors {
 		//relay information if needed
 		
 	}
+	
+	public void DriveFowardAuto(int Distance, int Speed) {
+		LeftFrontEnc.reset();
+		encoderDistanceReading = LeftFrontEnc.getDistance();
+		SmartDashboard.putNumber("encoder reading", encoderDistanceReading);
+		drive.driveCartesian(0, Speed, 0);
+		if(encoderDistanceReading > Distance) {
+			drive.driveCartesian(0, 0, 0);
+		}
+	    
+	}
+	
 
 }
