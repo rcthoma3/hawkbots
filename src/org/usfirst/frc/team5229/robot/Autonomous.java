92d1f31434e2f890fe3151e151413b8bc3374fd5
panugami000 <panugami000@HHS308-25.stcs.org>
2018-01-13 12:03:32 -0500

    Auto: Created Auto Class

---
+++ src/org/usfirst/frc/team5229/robot/Autonomous.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team5229.robot;
+
+public class Autonomous {
+	
+	
+	
+	
+	/*if (StartPosition = "Left") 
+	 
+	if (OurSwitchSide = "L")
+			
+	else if (OurSwitchSide = "R")
+	
+	
+else if (StartPosition = "Center")
+	if  (OurSwitchSide = "L")
+	
+	else if (OurSwitchSide = "R")
+else if (StartPosition = "Right")
+	if (OurSwitchSide = "L")
+	
+	else if (OurSwitchSide = "R")
+	
+(Cross line and deliver to switch or scale if switch is on the opposite side)
+if (StartPosition = "Left")
+	if (OurSwitchSide = "R" && ScaleSide = "R")
+	
+	else if (OurSwitchSide = "R" && ScaleSide = "L")
+	
+	else if (OurSwitchSide = "L")
+	
+else if (StartPosition = "Center")
+	if (OurSwitchSide = "L")
+	
+	else if (OurSwitchSide = "R")
+	
+else if (StartPosition = "Left")
+	if (OurSwitchSide = "L" && ScaleSide = "L")
+	
+	else if (OurSwitchSide = "L" && ScaleSide = "R")
+	
+	else if (OurSwitchSide = "R") 
+(Crossing line and delivering to Switch if Switch is handled by the center)
+if (StartPosition = "Left")
+	if (ScaleSide = "R" && OurSwitchSide = "R")
+	
+	else if (ScaleSide = "R" && OurSwitchSide = "L")
+	
+	else if (ScaleSide = "L")
+	
+else if (StartPosition = "Center")
+	
+	if (OurSwitchSide = "L")
+	
+	else if (OurSwitchSide = "R") 
+else if (StartPosition = "Right")
+	
+	if (ScaleSide = "L" && OurSwitchSide = "L")
+	
+	else if (ScaleSide = "L" && OurSwitchSide = "R")
+	
+	else if (ScaleSide = "R")*/
+	 
+
+}
