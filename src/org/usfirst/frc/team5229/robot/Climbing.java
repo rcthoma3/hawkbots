3bc8b63270f41cb1c957973d67717aaea383c0bb
gilbecon000 <gilbecon000@HHS308-06.stcs.org>
2018-01-13 12:49:59 -0500

    Added deploy hook function

---
+++ src/org/usfirst/frc/team5229/robot/Climbing.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team5229.robot;
+
+public class Climbing {
+	
+	//This function deploys hook
+	//Input of distance in feet
+	public void deployHook (double distance) {
+		
+		//Get distance
+		//Run motor until encoder count reaches "x" distance
+		
+		
+	}
+	
+}
