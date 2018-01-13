becf5c141e40e96a2be3c2921d94a0bb5f0399ee
gilbecon000 <gilbecon000@HHS308-06.stcs.org>
2018-01-13 14:13:27 -0500

    finished sensor code (Not that much)

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -2,6 +2,12 @@ package org.usfirst.frc.team5229.robot;
 
 public class Sensors {
 
-	
-	
+	//Gets robot position
+	public void getPosition (double distance) {
+		
+		//use sensor to figure out what starting position we are in
+		//relay information if needed
+		
+	}
+
 }
