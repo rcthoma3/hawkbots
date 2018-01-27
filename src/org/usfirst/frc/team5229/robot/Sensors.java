5186da7ce55db660d077522608c61d2e51c7a2ce
devosseb000 <devosseb000@HHS308-15.stcs.org>
2018-01-27 10:59:22 -0500

    Added converstions between dis and enc

--- src/org/usfirst/frc/team5229/robot/Sensors.java
+++ src/org/usfirst/frc/team5229/robot/Sensors.java
@@ -4,6 +4,9 @@ package org.usfirst.frc.team5229.robot;
 
 public class Sensors {
 
+	//Creates a public variable for encoders
+	public static int encTicksPerRot = 1440;
+	
 	//Gets robot position
 	//in:distance
 	//out:Nothing Yet
@@ -21,6 +24,37 @@ public class Sensors {
 	//out:nothing
 	public void DriveFowardAuto(int Distance, double speed) {
 
+	}
+	
+	//Converts Encoder variable to distance
+	//in: wheel size in inches and encoder counts
+	//out: distance traveled in inches 
+	public double EncToDis (double whlsize, int enc) {
+		
+		//Converts the diameter of the wheel to radius
+		//in inches
+		double r = whlsize / 2;
+		//Gets rotation from encoder value in inches
+		double rotations = enc / encTicksPerRot;
+		// Distance obtained from 2PIr multiplied
+		//By rotations in inches
+		double dis = 2*Math.PI*r * rotations;
+		return dis;
+	}
+	
+	//Converts distance(in inches) to a variable for
+	//in: wheel size in inches and desired distance in inches
+	//out: encoder counts
+	public int DisToEnc (double whlsize, double dis) {
+		
+		//Converts the diameter of the wheel to radius
+		//in inches
+		double r = whlsize / 2;
+		// Encoder variable obtained from 2PIr multiplied
+		//By rotations
+		int enc = (int) Math.round(((encTicksPerRot*dis)/( 2 * Math.PI * r)));
+		
+		return enc;
 	}
 
 
