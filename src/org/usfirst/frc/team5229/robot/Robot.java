524ea498513b6c7a62467f618fd8b401e23e030e
Hawkbots <Hawkbots@10.210.64.177>
2018-04-11 20:28:26 -0400

    BRAIIIIIIIIIIIIIIIIINNNNNNNNNNNNNNNNNNN!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/org/usfirst/frc/team5229/robot/Robot.java
+++ src/org/usfirst/frc/team5229/robot/Robot.java
@@ -1,11 +1,10 @@
+
 package org.usfirst.frc.team5229.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.AnalogTrigger;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -13,6 +12,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -24,24 +24,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 	
-	WPI_TalonSRX _testMotor;
-	//https://wpilib.screenstepslive.com/s/3120/m/7912/l/85635-using-counters
-	DigitalInput halSensor;
-	ControllerLogitech testController = new ControllerLogitech(1);
-	Boolean senPrev;
-	AnalogTrigger trigger;
-	int count;
-	
-	/*
 	// For Testing - Remove in final code
 	boolean forward = true;
 	boolean backward = true;
 	boolean turnRight = true;
 	boolean turnLeft = true;
 	boolean follow = true;
-
+	
 	ControllerLogitech myController = new ControllerLogitech(1); // input is usb value for drive station
-	//ControllerLogitech myDriveController = new ControllerLogitech(2);
+	ControllerLogitech myDriveController = new ControllerLogitech(2);
 	Climbing myClimber = new Climbing();
 	Elevator myElevator = new Elevator();
 	Sensors myRobot = new Sensors();
@@ -49,15 +40,15 @@ public class Robot extends IterativeRobot {
 	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 	
 	UsbCamera Camera1;
-	//UsbCamera Camera2;
+	UsbCamera Camera2;
 		
 	MecanumDrive _drive;
 	
 	// Motor Declarations 
-	WPI_TalonSRX _frontLeftMotor; 
-	WPI_TalonSRX _rearLeftMotor;
-	WPI_TalonSRX _frontRightMotor;
-	WPI_TalonSRX _rearRightMotor;
+	VictorSP _frontLeftMotor; 
+	VictorSP _rearLeftMotor;
+	VictorSP _frontRightMotor;
+	VictorSP _rearRightMotor;
 	WPI_TalonSRX _elevatorMotor;
 	WPI_TalonSRX _climbMotor;
 	VictorSP _leftClawMotor;
@@ -71,28 +62,32 @@ public class Robot extends IterativeRobot {
 
 	// These values correspond to roboRIO ports
 	//Talons - CAN
-	int frontLeftMotorPort = 5;
-	int rearLeftMotorPort = 6;
-	int frontRightMotorPort = 2;
-	int rearRightMotorPort = 3;
+	int frontLeftMotorPort = 6;
+	int rearLeftMotorPort = 5;
+	int frontRightMotorPort = 3;
+	int rearRightMotorPort = 2;
 	int elevatorMotorPort = 1;
 	int climbMotorPort = 4;
 	//Switches - DIO
-	int topElevatorPort = 0;
+	int topElevatorPort = 2;
 	int bottomElevatorPort = 1;
-	int topClimbPort =2;
+	int topClimbPort =0;
 	int bottomClimbPort = 3;
 	int grabSwitchPort = 4;
 	//Victors - PWM
 	int leftClawPort = 0;
 	int rightClawPort = 1;
 		
-	double whlSize = 8; // Wheel diameter in inches (Test Robot)
-	double roboDim = 30; // Diagonal distance between wheels in inches (Test Robot)
+	//double whlSize = 8; // Wheel diameter in inches (Test Robot)
+	//double roboDim = 30; // Diagonal distance between wheels in inches (Test Robot)
 	
-	//double whlSize = 8; // Wheel diameter in inches (Final Robot)
-	//double roboDim = 30; // Diagonal distance between wheels in inches (Final Robot)
-	*/
+	double whlSize = 8; // Wheel diameter in inches (Final Robot)
+	double roboDim = 30; // Diagonal distance between wheels in inches (Final Robot)
+	String gameMsg = "XXX";
+	
+
+	public SendableChooser<Boolean> autonTune;
+	public SendableChooser<Integer> autonAction;
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -100,34 +95,31 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		_testMotor = new WPI_TalonSRX(6);
-		halSensor = new DigitalInput(0);
-		trigger = new AnalogTrigger(3);
-		
 		/*
-		_frontLeftMotor = new WPI_TalonSRX(frontLeftMotorPort); 
-		_rearLeftMotor = new WPI_TalonSRX(rearLeftMotorPort);
-		_frontRightMotor = new WPI_TalonSRX(frontRightMotorPort);
-		_rearRightMotor = new WPI_TalonSRX(rearRightMotorPort);
-		
-		Camera1 = CameraServer.getInstance().startAutomaticCapture();
-		//Camera2 = CameraServer.getInstance().startAutomaticCapture();
-		
-		// Initialize to zero
-		_frontLeftMotor.set(0);
-		_rearLeftMotor.set(0);
-		_frontRightMotor.set(0);
-		_rearRightMotor.set(0);
-		
-		gyro.calibrate();
-		
-		myAutonRobot.setAutoChooser();
+		_frontLeftMotor = new VictorSP(frontLeftMotorPort); 
+		_rearLeftMotor = new VictorSP(rearLeftMotorPort);
+		_frontRightMotor = new VictorSP(frontRightMotorPort);
+		_rearRightMotor = new VictorSP(rearRightMotorPort);
 		
 		_climbMotor = new WPI_TalonSRX(climbMotorPort);
 		_leftClawMotor = new VictorSP(leftClawPort);
 		_rightClawMotor = new VictorSP(rightClawPort);
 		_elevatorMotor = new WPI_TalonSRX(elevatorMotorPort);
 		
+		Camera1 = CameraServer.getInstance().startAutomaticCapture();
+		Camera2 = CameraServer.getInstance().startAutomaticCapture();
+		
+		// Initialize to zero
+		_frontLeftMotor.set(0);
+		_rearLeftMotor.set(0);
+		_frontRightMotor.set(0);
+		_rearRightMotor.set(0);
+		_climbMotor.set(0);
+		_elevatorMotor.set(0);
+		
+		gyro.calibrate();
+		
+
 		topClimbSwitch = new DigitalInput(topClimbPort);
 		bottomClimbSwitch = new DigitalInput(bottomClimbPort);
 		topElevatorSwitch = new DigitalInput(topElevatorPort);
@@ -135,13 +127,54 @@ public class Robot extends IterativeRobot {
 		grabSwitch = new DigitalInput(grabSwitchPort);
 		
 		myClimber.setClimbMotor(_climbMotor);
+		myClimber.initElevator();
 		myClimber.setSwitches(topClimbSwitch, bottomClimbSwitch);
 		myElevator.setElevator(_elevatorMotor);
-		//myElevator.initElevator();
+		myElevator.initElevator();
 		myElevator.setSwitches(topElevatorSwitch, bottomElevatorSwitch, grabSwitch);
 		myElevator.setGrabMotors(_leftClawMotor, _rightClawMotor);
-		*/
-	}
+		
+		myRobot.setEncoders (_frontLeftMotor, _rearLeftMotor, _frontRightMotor,  _rearRightMotor);
+		myRobot.initEncoders();
+
+		autonTune = new SendableChooser<Boolean>();
+		autonTune.addDefault("Off", false);
+		autonTune.addObject("On", true);
+		SmartDashboard.putData("Auton Tune", autonTune);
+		
+		autonAction = new SendableChooser<Integer>();
+		autonAction.addDefault("Stop", 0);
+		autonAction.addObject("Drive Forward", 1);
+		autonAction.addObject("Drive Backward", 2);
+		autonAction.addObject("Turn Left", 3);
+		autonAction.addObject("Turn Right", 4);
+		autonAction.addObject("Raise Elevator", 5);
+		autonAction.addObject("Lower Elevator", 6);
+		autonAction.addObject("Grab Cube", 7);
+		autonAction.addObject("Eject Cube", 8);
+		autonAction.addObject("Open Claw", 9);
+		autonAction.addObject("Close Claw", 10);
+		autonAction.addObject("Tilt Claw Up", 11);
+		autonAction.addObject("Tilt Claw Up", 12);
+		SmartDashboard.putData("Auton Actions", autonAction);
+
+		myAutonRobot.setAutoChooser();
+		
+		SmartDashboard.putNumber("Forward Distance", 0);
+		SmartDashboard.putNumber("Backward Distance", 0);
+		SmartDashboard.putNumber("Turning Left", 0);
+		SmartDashboard.putNumber("Turning Right", 0);
+		SmartDashboard.putNumber("Raise Elevator", 0);
+		SmartDashboard.putNumber("Lower Elevator", 0);
+		SmartDashboard.putNumber("Grab Block", 0);
+		SmartDashboard.putNumber("Eject Block", 0);
+		SmartDashboard.putNumber("Lift Block", 0);
+		SmartDashboard.putNumber("Lower Block", 0);
+		SmartDashboard.putNumber("Open claws", 0);
+		SmartDashboard.putNumber("Close claws", 0);
+		
+		populateSmartDashboard();	
+	*/}
 	
 
 	/**
@@ -149,33 +182,22 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		
 		/*
-		myRobot.setEncoders (_frontLeftMotor, _rearLeftMotor, _frontRightMotor,  _rearRightMotor);
+		gameMsg = myAutonRobot.getGameMsg();
+		
 		myRobot.setWheelSize(whlSize);
 		myRobot.setChassisSize(roboDim);	
-		myRobot.initEncoders();
 		myRobot.setOverride(false);
 		myRobot.setGyro(gyro);
 		myAutonRobot.setSensor(myRobot);
-		String gameMsg = myAutonRobot.getGameMsg();
-		int pos = myAutonRobot.getPositoin();
-		
-		SmartDashboard.putString("Game message", gameMsg);
-		if(pos == 0) {
-			SmartDashboard.putString("Position", "Left");
-		}else if(pos == 1) {
-			SmartDashboard.putString("Position", "Center");
-		}else if(pos == 2) {
-			SmartDashboard.putString("Position", "Right");
-		}
-		
+		myAutonRobot.setElevator(myElevator);
+
 		// For Testing - Remove in final code
 		forward = true;
 		backward = true;
 		turnRight = true;
 		turnLeft = true;
-		follow = true;
+		follow = true;	
 		*/
 	}
 
@@ -184,16 +206,17 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {	
-		
-		//if (forward) {System.out.println("Going Forward"); forward = !myRobot.driveFowardAuto(120); System.out.println("Done Going Forward"); myRobot.stopRobot(); Timer.delay(0.010);}
+		/*
+		//if (forward) {System.out.println("Going Forward"); forward = !myRobot.driveFowardAuto(240); System.out.println("Done Going Forward"); myRobot.stopRobot(); }
 		//if (backward) {System.out.println("Going Backward"); backward = !myRobot.driveBackwardAuto(120); System.out.println("Done Going Backward"); myRobot.stopRobot(); Timer.delay(0.010);}
-		//if (turnRight) {System.out.println("Doing Rigt Turn"); turnRight = !myRobot.turnRobotRight(90); System.out.println("Done Turning Right"); myRobot.stopRobot(); Timer.delay(0.010);}
-		//if (turnLeft) {System.out.println("Doing Left Turn"); turnLeft = !myRobot.turnRobotLeft(90); System.out.println("Done Turning Left"); myRobot.stopRobot(); Timer.delay(0.010);}
+		//if (turnRight) {System.out.println("Doing Right Turn"); turnRight = !myRobot.turnRobotRightGyro(90); System.out.println("Done Turning Right"); myRobot.stopRobot(); Timer.delay(0.010);}
+		//if (turnLeft) {System.out.println("Doing Left Turn"); turnLeft = !myRobot.turnRobotLeftGyro(90); System.out.println("Done Turning Left"); myRobot.stopRobot(); Timer.delay(0.010);}
 		//if(follow) { follow = !myAutonRobot.followPath(); System.out.println("Done"); }
 		//if (turnLeft) {System.out.println("Doing Left Turn"); turnLeft = !myRobot.turnRobotLeftGyro(90); System.out.println("Done Turning Left"); myRobot.stopRobot(); Timer.delay(0.010);}
 		//myRobot.stopRobot();
 		
 		Timer.delay(0.005);
+	*/
 	}
 
 	/**
@@ -202,69 +225,73 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopInit() {	
-		/*
+		
+		myRobot.setOverride(true);
+		myRobot.stopRobot();
+		follow = false;
+		
 		// Initialize mecanum drive
 		_drive = new MecanumDrive(_frontLeftMotor, _rearLeftMotor, _frontRightMotor, _rearRightMotor);		
 		
 		// Something to do with safety 
 		_drive.setSafetyEnabled(false);
 		_drive.setExpiration(0.1);
-		
+		w
 		// Set Max output
-		_drive.setMaxOutput(0.6);	
+		_drive.setMaxOutput(1.0);	
 		
 		// Motors should not be inverted for WPILib MecanumDrive Function
 		_frontRightMotor.setInverted(false);
 		_rearRightMotor.setInverted(false);
 		_frontLeftMotor.setInverted(false);
-		_rearLeftMotor.setInverted(false);	
-		*/
-		count = 0;
-		senPrev = trigger.getInWindow();		
-		trigger.setLimitsVoltage(3.5, 5.0);
+		_rearLeftMotor.setInverted(false);
+		
+		populateSmartDashboard();
+		
 	}
 
 	/**
 	 * This function is called periodically during operator control 
 	 */
 	@Override
-	public void teleopPeriodic() {
-		boolean sen = trigger.getInWindow();
-		if(testController.getButtonA()) { 
-			_testMotor.set(1); 
-			if(sen!=senPrev) {count++; senPrev=sen;}
-		}
-		else if(testController.getButtonB()) { 
-			_testMotor.set(-1); 
-			if(sen!=senPrev) {count--; senPrev=sen;}
-		}
-		else { _testMotor.set(0); }
+	public void teleopPeriodic()
+	{
 		
-		
+		if(!autonTune.getSelected())
+		{
+			_drive.driveCartesian(myDriveController.getLeftJoyX(), -myDriveController.getLeftJoyY(), myDriveController.getRightJoyX(), 0);	
+			/*
+			if (myDriveController.getButtonLeftBumber() && myDriveController.getButtonRightBumber()) { _drive.setMaxOutput(.5); }
+			else if (myDriveController.getButtonLeftBumber()) { _drive.setMaxOutput(.25); }
+			else if (myDriveController.getButtonRightBumber()) { _drive.setMaxOutput(.25); }
+			else { _drive.setMaxOutput(1.0); }
 			
-		
-		System.out.println(sen);
-		
-		System.out.println(count);
-		/*	
-		_drive.driveCartesian(myController.getLeftJoyX(), -myController.getLeftJoyY(), myController.getRightJoyX(), 0);	
-		
-		if (myController.getButtonY()) { myClimber.raiseElevator(1, true); }
-		if (myController.getButtonA()) { myClimber.lowerElavator(1, true); }
-		
-		if (myController.getButtonUpD() ) { myClimber.raiseElevator(1, false); }
-		else if (myController.getButtonDownD() ) { myClimber.lowerElavator(1, false); }
-		else { myClimber.checkSwitches(false); }
-		
-		if (myController.getButtonX()) { myElevator.raiseElevator(0.3, true); }
-		if (myController.getButtonB()) { myElevator.lowerElevator(0.3, true); }
-		
-		if (myController.getButtonLeftBumber()) { myElevator.grabBlock(1); }
-		else if (myController.getButtonRightBumber()) { myElevator.ejectBlock(1); }
-		else { myElevator.ejectBlock(0); }
-		*/
-		
+			if (myController.getButtonRightD()) { myClimber.raiseElevator(1200, true); }
+			if (myController.getButtonLeftD()) { myClimber.lowerElavator(400, true); }
+			
+			if (myController.getButtonUpD() ) { myClimber.raiseElevator(1200, false); }
+			else if (myController.getButtonDownD() ) { myClimber.lowerElavator(400, false); }
+			else { myClimber.checkSwitches(false); }
+			
+			
+			if (myController.getButtonA()) { myElevator.lowerElevatorDis(0); }
+			if (myController.getButtonX()) { myElevator.raiseElevatorDis(20000); } //Switch height
+			if (myController.getButtonY()) { myElevator.raiseElevatorDis(70000); } //Scale height
+			
+			if (myController.getLeftJoyY() < -0.1) { myElevator.raiseElevator(myController.getLeftJoyY()*-600, false); }
+			else if (myController.getLeftJoyY() > 0.1) { myElevator.lowerElevator(myController.getLeftJoyY()*600, false); }
+			else { myElevator.checkSwitches(false); }
+			
+			if (myController.getButtonLeftBumber()) { myElevator.grabBlock(1); }
+			else if (myController.getButtonRightBumber()) { myElevator.ejectBlock(1); }
+			else { myElevator.ejectBlock(0); }
+		*/}
+		else { autonTune(); }
+	
+		populateSmartDashboard();
+
 		Timer.delay(0.005);
+		
 	}
 
 	/**
@@ -278,7 +305,122 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void disabledInit() {
-		//myRobot.setOverride(true);
-		//myRobot.stopRobot();
+		
+	/*	myRobot.setOverride(true);
+		myRobot.stopRobot();
+		follow = false;
+		*/
 	}
+	
+	public void populateSmartDashboard ()
+	{ /*
+		SmartDashboard.putNumber("Climb Motor Current", _climbMotor.getOutputCurrent());
+		SmartDashboard.putNumber("Climb Motor Voltage", _climbMotor.getMotorOutputVoltage());
+		SmartDashboard.putNumber("Climb Motor Percent Output", _climbMotor.getMotorOutputPercent());
+		SmartDashboard.putNumber("Climb Position", _climbMotor.getSelectedSensorPosition(0));
+		SmartDashboard.putNumber("Climb Velocity", _climbMotor.getSelectedSensorVelocity(0));
+		SmartDashboard.putBoolean("Climb Max Height", myClimber.getTopSwitch());
+		SmartDashboard.putBoolean("Climb Min Height", myClimber.getBottomSwitch());
+		
+		SmartDashboard.putNumber("Elevator Motor Current", _elevatorMotor.getOutputCurrent());
+		SmartDashboard.putNumber("Elevator Motor Voltage", _elevatorMotor.getMotorOutputVoltage());
+		SmartDashboard.putNumber("Elevator Motor Percent Output", _elevatorMotor.getMotorOutputPercent());
+		SmartDashboard.putNumber("Elevator Position", _elevatorMotor.getSelectedSensorPosition(0));
+		SmartDashboard.putNumber("Elevator Veocity", _elevatorMotor.getSelectedSensorVelocity(0));
+		SmartDashboard.putBoolean("Elevator Max Height", myElevator.getElevatorTop());
+		SmartDashboard.putBoolean("Elevator Min Height", myElevator.getElevatorBottom());
+		SmartDashboard.getNumber("Foward Dis", 5);
+	
+		SmartDashboard.putString("Game message", gameMsg);
+		
+		int pos = myAutonRobot.getPositoin();
+		if(pos == 0) {
+			SmartDashboard.putString("Position", "Left");
+		}else if(pos == 1) {
+			SmartDashboard.putString("Position", "Center");
+		}else if(pos == 2) {
+			SmartDashboard.putString("Position", "Right");
+		}
+		
+		int goal = myAutonRobot.getGlobalGoal();
+		if(goal == 0) {
+			SmartDashboard.putString("Goal", "Switch");
+		}else if(goal == 1) {
+			SmartDashboard.putString("Goal", "Scale");
+		}else if(goal == 2) {
+			SmartDashboard.putString("Goal", "Neither");
+		}
+		else {SmartDashboard.putString("Goal", "Error");}
+		
+		myRobot.updateDashboard();
+		
+		int avgDis = (_frontLeftMotor.getSelectedSensorPosition(0) + _rearLeftMotor.getSelectedSensorPosition(0)+
+					_frontRightMotor.getSelectedSensorPosition(0) + _rearRightMotor.getSelectedSensorPosition(0))/4;
+		SmartDashboard.putNumber("Avg Dis", myRobot.encToDis(avgDis));
+		SmartDashboard.putNumber("Gyro", gyro.getAngle());
+			
+	}
+	
+	public void autonTune() {
+		
+		int autonChoice = autonAction.getSelected();
+		
+		switch(autonChoice) {
+	    
+		case 1:
+			myRobot.driveFowardAuto((int)SmartDashboard.getNumber("Forward Distance", 0));
+			myRobot.stopRobot();
+			break;
+		case 2:
+			myRobot.driveBackwardAuto((int)SmartDashboard.getNumber("Backward Distance", 0));
+			myRobot.stopRobot();
+			break;
+		case 3:			
+			myRobot.turnRobotLeftGyro(SmartDashboard.getNumber("Turning Left", 0));
+			myRobot.stopRobot();
+			break;
+		case 4:			
+			myRobot.turnRobotRightGyro(SmartDashboard.getNumber("Turning Right", 0));
+			myRobot.stopRobot();
+			break;
+		case 5:			
+			myElevator.raiseElevatorDis(SmartDashboard.getNumber("Raise Elevator", 0));
+			myRobot.stopRobot();
+			break;
+		case 6:			
+			myElevator.lowerElevatorDis(SmartDashboard.getNumber("Lower Elevator", 0));
+			myRobot.stopRobot();
+			break;
+		case 7:			
+			myElevator.grabBlock(SmartDashboard.getNumber("Grab Block", 0));
+			myRobot.stopRobot();
+			break;
+		case 8:			
+			myElevator.ejectBlock(SmartDashboard.getNumber("Eject Block", 0));
+			myRobot.stopRobot();
+			break;
+		case 9:			
+			SmartDashboard.getNumber("Lift Block", 0);
+			myRobot.stopRobot();
+			break;
+		case 10:			
+			SmartDashboard.getNumber("Lower Block", 0);
+			myRobot.stopRobot();
+			break;
+		case 11:			
+			SmartDashboard.getNumber("Open claws", 0);
+			myRobot.stopRobot();
+			break;
+		case 12:			
+			SmartDashboard.getNumber("Close claws", 0);
+			myRobot.stopRobot();
+			break;
+	    default:
+	    	myRobot.stopRobot();
+			myElevator.raiseElevatorDis(0);
+			myElevator.grabBlock(0);	
+			break;
+	    */
+		}
+	}	
 }
